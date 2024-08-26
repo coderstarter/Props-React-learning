16a83ba8ce98d8c167fbffe61422f11a3563ba3e
Abhishek Sharma <102434201+coderstarter@users.noreply.github.com>
2024-08-27 03:20:49 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,33 @@
-import { useState } from 'react';
 import './App.css';
-import Counter from './Counter';
+import { useState } from 'react';
 function App() {
   const [count, setCount] = useState(0);
+  function handleIncrement() {
+    setCount(count +1);
+    setCount(count+1)
+    // The function handleIncrement is incrementing the value by 1 
+    // instead of 2 because setCount is asynchronous and does not immediately
+    // update the state. When you call setCount(count + 1) twice in a row, both
+    //  calls are using the same initial value of count, so the second call does
+    //  not see the updated value from the first call.
+    setCount((count) => count+1);
+    setCount((count) => count+1);
+    setCount((count) => count+1);
+    setCount((count) => count+1);
+    // By using the functional form of setCount, each update is based on the 
+    // previous state, ensuring that the state is correctly incremented.
+  }
+  function handleDecrement(){
+    if(count === 0){
+      return;
+    }
+    setCount((count) => count-1);
+  }
   return (
     <div className="App">
-      <Counter update={setCount} text={count}/>
-      <Counter update={setCount} text={count}/>
-      <Counter update={setCount} text={count}/>
-      <button onClick={() => setCount(count+1)}>Increment +</button>
+      <button onClick={handleIncrement}>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={handleDecrement}>Decrement</button>
     </div>
   );
 }
